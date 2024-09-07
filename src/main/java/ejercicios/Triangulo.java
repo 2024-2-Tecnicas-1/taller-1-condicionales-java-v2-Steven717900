019e70a1440b03fa953c98ce5da5f1b3aa827ad3
Steven <Pospinachico@gmail.com>
2024-09-07 18:41:44 -0500

    Solución Triangulo

--- src/main/java/ejercicios/Triangulo.java
+++ src/main/java/ejercicios/Triangulo.java
@@ -10,7 +10,21 @@ import java.util.Scanner;
 public class Triangulo {
     public static String evaluar(double a, double b, double c) {
         // TODO: Coloca aquí el código del ejercicio 7: Triángulos
-        return "";
+       String respuesta="";
+        if (a<(b+c) && b<(a+c) && c<(b+a) ){
+            if(a==b && a!=c || a==c && b!=c|| b==c && a!=c){
+                respuesta= "El triángulo es isósceles";
+            }
+            else if(a!=c && b!=c && a!=b){
+                       respuesta= "El triángulo es escaleno";     
+                            }
+            else if(a==c && b==c && a==b){
+                          respuesta= "El triángulo es equilátero";      
+                                }
+        }else{
+            respuesta= "No es un triángulo válido";
+        }
+        return respuesta;
     }
     
     public static void main(String[] args) {
