5379be593011ecf09d6de4ea5dadc1a635fadf17
Steven <Pospinachico@gmail.com>
2024-09-07 18:28:32 -0500

    Se agrega la solucion de años bisiestos

--- src/main/java/ejercicios/AnnosBisiestos.java
+++ src/main/java/ejercicios/AnnosBisiestos.java
@@ -1,4 +1,3 @@
-
 package ejercicios;
 
 import static ejercicios.SetDeTenis.evaluar;
@@ -9,18 +8,34 @@ import java.util.Scanner;
  * @author danielsanchez
  */
 public class AnnosBisiestos {
-    public static String evaluar(int anno){
+
+    public static String evaluar(int anno) {
         // TODO: Coloca aquí el código del ejercicio 2: Años bisiestos
-        return "";
+        String respuesta = "";
+        if (anno >= 1582) {
+            if (anno % 400 == 0 || (anno % 4 == 0 && anno % 100 != 0)) {
+                respuesta = anno + " es bisiesto";
+            } else {
+                respuesta = anno + " no es bisiesto";
+            }
+        } else {
+            if (anno % 4 == 0) {
+                respuesta = anno + " es bisiesto";
+            } else {
+                respuesta = anno + " no es bisiesto";
+            }
+
+        }
+        return respuesta;
     }
-    
+
     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
         System.out.print("Año:");
         int anno = lector.nextInt();
-        
+
         String respuesta = evaluar(anno);
         System.out.println(respuesta);
     }
-            
+
 }
