57950c7e2118980f7b7e30b9d4cdd3738c2f2ec5
Steven <Pospinachico@gmail.com>
2024-09-07 18:39:02 -0500

    Solucion ordenamiento

--- src/main/java/ejercicios/Ordenamiento.java
+++ src/main/java/ejercicios/Ordenamiento.java
@@ -1,20 +1,29 @@
-
 package ejercicios;
 
 import static ejercicios.SetDeTenis.evaluar;
 import java.util.Scanner;
+import java.util.Arrays;
 
 /**
  *
  * @author danielsanchez
  */
 public class Ordenamiento {
+
     public static String evaluar(int numero1, int numero2, int numero3, int numero4) {
         // TODO: Coloca aquí el código del ejercicio 5: Ordenamiento
-        return "";
+        int[] numeros = {numero1, numero2, numero3, numero4};
+        Arrays.sort(numeros);
+
+        StringBuilder respuesta = new StringBuilder();
+        for (int n : numeros) {
+            respuesta.append(n).append(" ");
+        }
+        
+        return respuesta.toString().trim();
     }
-    
-    public static void main(String[] args) {
+
+     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
         System.out.print("Número 1:");
         int numero1 = lector.nextInt();
@@ -24,7 +33,7 @@ public class Ordenamiento {
         int numero3 = lector.nextInt();
         System.out.print("Número 4:");
         int numero4 = lector.nextInt();
-        
+
         String respuesta = evaluar(numero1, numero2, numero3, numero4);
         System.out.println(respuesta);
     }
