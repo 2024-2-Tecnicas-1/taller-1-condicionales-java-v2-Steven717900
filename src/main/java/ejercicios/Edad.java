60ef27340207edc2c427f5c33106bbeb0b7db012
Steven <Pospinachico@gmail.com>
2024-09-07 18:31:45 -0500

    solución edad

--- src/main/java/ejercicios/Edad.java
+++ src/main/java/ejercicios/Edad.java
@@ -1,7 +1,7 @@
-
 package ejercicios;
 
 import static ejercicios.SetDeTenis.evaluar;
+import java.time.LocalDate;
 import java.util.Scanner;
 
 /**
@@ -11,9 +11,18 @@ import java.util.Scanner;
 public class Edad {
     public static String evaluar(int dia, int mes, int anno) {
         // TODO: Coloca aquí el código del ejercicio 6: Edad
-        return "";
+        LocalDate currentDate = LocalDate.now();
+        LocalDate nacimiento = LocalDate.of(anno, mes, dia);
+
+        int edad = currentDate.getYear() - nacimiento.getYear();
+
+        if (currentDate.getMonthValue() <= nacimiento.getMonthValue() && currentDate.getDayOfMonth() < nacimiento.getDayOfMonth()) {
+            edad--;
+        }
+ String respuesta = "Usted tiene " + edad + " años";
+        return respuesta;
     }
-    
+
     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
         System.out.println("Ingrese su fecha de nacimiento.");
@@ -23,7 +32,7 @@ public class Edad {
         int mes = lector.nextInt();
         System.out.print("Año:");
         int anno = lector.nextInt();
-        
+
         String respuesta = evaluar(dia, mes, anno);
         System.out.println(respuesta);
     }
